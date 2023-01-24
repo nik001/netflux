f9eefce806d2371883486c672219fe675a22e000
nikolay <A6oltyc@list.ru>
2023-01-25 00:04:20 +0300

    text text

--- src/Content/MainContent/SectionOneSlider/SectionOneSlider.js
+++ src/Content/MainContent/SectionOneSlider/SectionOneSlider.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './SectionOneSlider.css'
 import foto from './foto.png';
+import VectorLeft from './VectorLeft.png';
+import VectorRight  from './VectorRight.png';
+import VectoEclipsLoad  from './Ellipse-slider.png';
 
 const SectionOneSlider = () => {
     return (
         <div className='Main-slider'>
             <div className='Slider-S'>
+                <div className='vec-left'> 
+                    <img className='vec-img' src={VectorLeft} alt='img' />
+                </div>
 
                 <div className='Slider-text'>
                    <h2 className='Slider-text-Title'>НАША НАДЕЖНАЯ КОМАНДА IT-ЭКСПЕРТОВ  
@@ -32,9 +38,22 @@ const SectionOneSlider = () => {
                    
                 </div>
                 <div className='Slider-s-images'>
-                    <img src={foto} alt='img' />
+                    <img clas src={foto} alt='img' />
+                </div>
+                <div className='vec-right'> 
+                    <img className='vec-img' src={VectorRight} alt='img' />
                 </div>
 
+            </div>
+
+            <div className='slider-shag'>
+                <div className='shag-center'>
+                    <img className='vec-img-load' src={VectoEclipsLoad} alt='img' />
+                    <img className='vec-img-load' src={VectoEclipsLoad} alt='img' />
+                    <img className='vec-img-load' src={VectoEclipsLoad} alt='img' />
+                    <img className='vec-img-load' src={VectoEclipsLoad} alt='img' />
+                </div>
+            
             </div>
         </div>
     );
